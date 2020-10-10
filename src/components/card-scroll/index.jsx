65b37f43940aaa7fe507af29b98326ef0d840c95
Abner Machado <abner81@live.com>
2020-10-10 19:53:19 -0300

    fix: package.json

--- src/components/card-scroll/index.jsx
+++ src/components/card-scroll/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import styles from './styles'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 export default function CardScroll() {
   return (
     <View style={styles.cardWrapper}>
+      <Icon name="hands-helping" size={13} color="#a55eea" />
       <Text style={styles.textInfo}>Design Marketing</Text>
     </View>
   )
