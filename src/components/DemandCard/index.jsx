fcbd85d7a062c2b7413dddd0a41cdee82a053e7a
victorlss <victor.souza@walmart.com>
2020-10-12 14:42:37 -0300

    Connect ServiceDetail to store

--- src/components/DemandCard/index.jsx
+++ src/components/DemandCard/index.jsx
@@ -1,36 +1,45 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import {View, Text, TouchableOpacity} from "react-native";
 import styles from "./styles";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import {connect} from "react-redux";
+import {setService} from "../../store/actions/serviceActions";
 
-export default function DemandCard(props) {
-  const {service, business, onPress, navigate} = props
+const services = [
+  {
+    name: "Design Marketing",
+    icon: "hands-helping",
+    id: "design-marketing",
+    services: ["Logo", "Capa para Instagram", "Plano de Marketing"],
+  },
+  {
+    name: "Planejamento de Negócio",
+    icon: "hands-helping",
+    id: "planejamento-negocio",
+    services: ["Plano de Negócio", "Precificação", "Estratégia de Negocio"],
+  },
+  {
+    name: "Contabilidade",
+    icon: "hands-helping",
+    id: "contabilidade",
+    services: ["Controle de Caixa", "Previsao de Lucro", "Impostos e Taxas"],
+  }
+]
+
+function DemandCard(props) {
+  const {service, business, navigate} = props
 
   const isBusiness = business !== undefined;
   const style = styles(isBusiness);
-  let services = [
-    {
-      name: "Design Marketing",
-      icon: "hands-helping",
-      id: "design-marketing",
-      services: ["Logo", "Capa para Instagram", "Plano de Marketing"],
-    },
-    {
-      name: "Planejamento de Negócio",
-      icon: "hands-helping",
-      id: "planejamento-negocio",
-      services: ["Plano de Negócio", "Precificação", "Estratégia de Negocio"],
-    },
-    {
-      name: "Contabilidade",
-      icon: "hands-helping",
-      id: "contabilidade",
-      services: ["Controle de Caixa", "Previsao de Lucro", "Impostos e Taxas"],
-    },
-  ];
-  const details = services.filter((x) => x.id == service.serviceId)[0].services;
+  const details = services.filter((x) => x.id === service.serviceId)[0].services;
+
+  const requestService = () => {
+    props.setService({...service, services: details})
+    navigate()
+  }
+
   return (
-    <TouchableOpacity style={style.containerWrapper} onPress={onPress}>
+    <View style={style.containerWrapper}>
       <View style={style.leftCard}>
         <Text style={style.strongText}>{service.professional.name}</Text>
         <Text style={style.text}>{service.description}</Text>
@@ -38,16 +47,22 @@ export default function DemandCard(props) {
       </View>
       <View style={style.rigthCard}>
         <Text style={style.strongText}>Serviços</Text>
-        {details.map((detail) => (
-          <Text style={style.text}>{detail}</Text>
+        {details.map((detail, index) => (
+          <Text key={index} style={style.text}>{detail}</Text>
         ))}
-        <TouchableOpacity onPress={() => navigate()}>
+        <TouchableOpacity onPress={() => requestService()}>
           <Text style={style.linkText}>
-            <Icon name="check" size={13} color={style.linkText.color} />{" "}
+            <Icon name="check" size={13} color={style.linkText.color}/>{" "}
             Solicitar serviço
           </Text>
         </TouchableOpacity>
       </View>
-    </TouchableOpacity>
+    </View>
   )
 }
+
+const mapDispatchToProps = {
+  setService: setService,
+}
+
+export default connect(null, mapDispatchToProps)(DemandCard);
--- src/pages/Business/ServiceDetail/index.jsx
+++ src/pages/Business/ServiceDetail/index.jsx
@@ -1,43 +1,56 @@
 import React from 'react'
-import { CheckBox, Text, View } from 'react-native'
-import ProfileHeader from '../../../components/ProfileHeader'
+import { Text, View } from 'react-native'
 import ServicesCard from '../../../components/ServicesCard'
 import Stars from '../../../components/Stars'
 import styles from './styles'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import LargeButton from '../../../components/LargeButton'
+import {connect} from "react-redux";
 
-export default function ServiceDetail() {
+const serviceCard ={
+  'design-marketing': (
+    <ServicesCard
+      icon={<Icon name="pencil-alt" size={20} color="#a55eea" />}
+      text="Design Marketing"
+    />
+  ),
+  'planejamento-negocio': (
+    <ServicesCard
+      icon={<Icon name="user-tie" size={20} color="#a55eea" />}
+      text="Planejamento de Negócio"
+    />
+  ),
+  'contabilidade': (
+    <ServicesCard
+      icon={<Icon name="calculator" size={20} color="#a55eea" />}
+      text="Contabilidade"
+    />
+  )
+}
+
+function ServiceDetail(props) {
+  const {professional, serviceId, services} = props.service
+
   return (
     <View style={styles.containerWrapper}>
-      <ProfileHeader />
       <View style={styles.servicesWrapper}>
-        <ServicesCard
-          icon={<Icon name="pencil-alt" size={20} color="#a55eea" />}
-          text="Design Marketing"
-        />
+        {serviceCard[serviceId]}
       </View>
       <View style={styles.ProfileInfoWrapper}>
-        <Text style={styles.strongText}>Priscila alvez</Text>
-        <Text style={styles.text}>(67) 1456346-45</Text>
-        <Text style={styles.text}>priscila.design@gmail.com</Text>
+        <Text style={styles.strongText}>{professional.name}</Text>
+        <Text style={styles.text}>{professional.telefone}</Text>
+        <Text style={styles.text}>{professional.email}</Text>
       </View>
       <View style={styles.servicesViewWrapper}>
         <Text style={styles.textLabel}>Serviços</Text>
-        <View style={styles.intraServicesWrapper}>
-          <CheckBox />
-          <Text style={styles.checkLabel}>Logomarca</Text>
-        </View>
-        <View style={styles.intraServicesWrapper}>
-          <CheckBox />
-          <Text style={styles.checkLabel}>Arte para instagram</Text>
-        </View>
-        <View style={styles.intraServicesWrapper}>
-          <CheckBox />
-          <Text style={styles.checkLabel}>Assessoria de engajamento</Text>
-        </View>
+        {services.map((service, index) => (
+          <View key={index} style={styles.intraServicesWrapper}>
+          <Text style={styles.checkLabel}>{service}</Text>
+          </View>
+        ))}
       </View>
-      <LargeButton text="SOLICITAR SERVIÇO" />
+      <LargeButton text="SOLICITAR SERVIÇO" business />
+
       <View style={styles.avaliationsWrapper}>
         <Text style={[styles.textLabel, styles.starsWrapper]}>Avaliações</Text>
         <Stars />
@@ -68,3 +81,11 @@ export default function ServiceDetail() {
     </View>
   )
 }
+
+const mapStateToProps = function (state) {
+  return {
+    service: state.service
+  }
+}
+
+export default connect(mapStateToProps)(ServiceDetail);
