9825f33c6af94d64d80f9e5091852b506bb14eb9
victorlss <victor.souza@walmart.com>
2020-10-12 00:33:13 -0300

    Fix conflicts

--- src/routes/BusinessNavigation.jsx
+++ src/routes/BusinessNavigation.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import AddCard from '../pages/Business/AddCard'
 import Home from '../pages/Business/Home'
 import Profile from '../pages/Business/Profile'
 import Services from '../pages/Business/Services'
-<<<<<<< HEAD
 import Login from "../pages/Login";
 import colors from "../styles/colors";
-=======
-import AddCard from '../pages/Business/AddCard'
->>>>>>> feat/payments-page
 
 function ProfessionalNavigation(props) {
   const {Navigator, Screen} = createBottomTabNavigator()
   return (
-    <Navigator
-      tabBarOptions={{
-        activeTintColor: colors.business.primary
-      }}
-    >
+    <Navigator tabBarOptions={{activeTintColor: colors.business.primary}}>
       <Screen
         name="Home"
         component={Home}
         options={{
           tabBarLabel: 'Home',
-          tabBarIcon: ({color}) => {
-            return <Icon name="home" size={18} color={color}/>
-          }
+          tabBarIcon: ({color}) => <Icon name="home" size={18} color={color}/>
         }}
       />
-<<<<<<< HEAD
 
       {props.user.isLoggedIn ? (
         <Screen
@@ -52,48 +42,27 @@ function ProfessionalNavigation(props) {
         />
       )}
 
-=======
-      <Screen
-        name="Entrar"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Entrar',
-          tabBarIcon: ({color}) => {
-            return <Icon name="user-circle" size={18} color={color}/>
-          }
-        }}
-      />
-
       <Screen
         name="Payments"
         component={AddCard}
         options={{
           tabBarLabel: 'Pagamento',
-          tabBarIcon: ({color}) => {
-            return <Icon name="credit-card" size={18} color={color} />
-          }
+          tabBarIcon: ({color}) => <Icon name="credit-card" size={18} color={color}/>
         }}
       />
->>>>>>> feat/payments-page
       <Screen
         name="Services"
         component={Services}
         options={{
           tabBarLabel: 'Serviços',
-          tabBarIcon: ({color}) => {
-<<<<<<< HEAD
-            return <Icon name="toolbox" size={18} color={color} />
-=======
-            return <Icon name="shopping-bag" size={18} color={color} />
->>>>>>> feat/payments-page
-          }
+          tabBarIcon: ({color}) => <Icon name="toolbox" size={18} color={color}/>
         }}
       />
     </Navigator>
   )
 }
 
-const mapStateToProps = function(state) {
+const mapStateToProps = function (state) {
   return {
     user: state.user
   }
