5918c837d2750545b4e4762c355ba31dbf8de8f6
victorlss <victor.souza@walmart.com>
2020-10-12 14:40:51 -0300

    Add service store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers } from 'redux';
+import serviceReducer from "./reducers/serviceReducer";
 import userReducer from './reducers/userReducer'
 
 const reducers = combineReducers({
+  service: serviceReducer,
   user: userReducer
 });
 
