9d307bfad2044a9951965c2b2e176a2fab6a48d4
victorlss <victor.souza@walmart.com>
2020-10-12 00:41:30 -0300

    Add secureTextEntry on password field

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -34,12 +34,14 @@ export default function Login(props) {
 
       <View style={styles.formView}>
         <TextInput
-          placeholder="Email"
+          placeholder='Email'
+          keyboardType='email-address'
           style={styles.textInput}
           onChangeText={(text) => setUsername(text)}
         />
         <TextInput
-          placeholder="Senha"
+          placeholder='Senha'
+          secureTextEntry={true}
           style={styles.textInput}
           onChangeText={(text) => setPassword(text)}
         />
